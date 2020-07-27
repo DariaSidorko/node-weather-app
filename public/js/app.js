c7c258bd6d874ec5fb3af83ad5f7d02ae7169218
DaraiSidorko <daria.sidorko@gmail.com>
2020-07-27 15:13:57 -0700

    ui update

--- public/js/app.js
+++ public/js/app.js
@@ -4,21 +4,44 @@ const search = document.querySelector('input');
 
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const temp = document.querySelector('#temp')
+const feelslike = document.querySelector('#feelslike')
+const humidity = document.querySelector('#humidity')
+const wind_speed = document.querySelector('#wind_speed')
+const precip = document.querySelector('#precip')
+const timezone = document.querySelector('#timesone')
+
+//const weather_icon = document.querySelector('#weather_icon')
+const icon = document.createElement("img");
+icon.className = "icon";
+//const weather_icon = document.querySelector('#weather_icon')
+
+//'https://media.geeksforgeeks.org/wp-content/uploads/20190529122826/bs11.png';
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const location = search.value
+  const location = search.value;
 
   fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
-        console.log(data.error)
+        console.log(data.error);
       } else {
-        messageOne.textContent = data.location
-        messageTwo.textContent = data.forecast.message
-        //console.log(data.location)
-        console.log(data.forecast)
+        console.log(data.forecast.weather_icon);
+        messageOne.textContent = data.location;
+        //messageTwo.textContent = data.forecast.message;
+        
+        icon.src = data.forecast.weather_icon;
+        document.getElementById("weather_icon").appendChild(icon);
+
+        temp.textContent = data.forecast.temp;
+        //feelslike.textContent = data.feelslike;
+        humidity.textContent = "Humidity: " + data.forecast.humidity + '%';
+        wind_speed.textContent = "Wind speed: " + data.forecast.wind_speed + ' mph';
+        precip.textContent = "Precipitation: " + data.forecast.precip + '%';
+        //timezone.textContent = data.forecast.timezone;
+        console.log(data.forecast);
       }
     })
   })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,7 +5,7 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=5dd219bcd6abc330f37a076117e66b3c&query=' + latitude + ',' + longitude + '&units=f';
     request({ url, json: true }, (error, {body}) => {
-    //console.log(response.body.current)
+    console.log(body)
     if (error) {
       callback('Unable to connect to weather service!')
     } else if (body.error) {
@@ -14,8 +14,13 @@ const forecast = (latitude, longitude, callback) => {
       callback(undefined, {
         message: body.current.weather_descriptions[0] + ". It's currently " + body.current.temperature + " degrees out.",
         description: body.current.weather_descriptions[0],
-        temp: body.current.temperature,
-        feelslike: body.current.feelslike
+        temp: body.current.temperature + 'F/' + body.current.feelslike + 'F',
+        feelslike: body.current.feelslike,
+        humidity: body.current.humidity,
+        wind_speed: body.current.wind_speed,
+        precip: body.current.precip,
+        weather_icon: body.current.weather_icons[0],
+        timezone: body.location.timezone_id
       })
     }
   })
