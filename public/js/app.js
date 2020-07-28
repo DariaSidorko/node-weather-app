d8f1b4935176e3abaff3d3c276214a7176537d9c
DaraiSidorko <daria.sidorko@gmail.com>
2020-07-28 11:31:29 -0700

    another UI update

--- public/js/app.js
+++ public/js/app.js
@@ -11,40 +11,36 @@ const wind_speed = document.querySelector('#wind_speed')
 const precip = document.querySelector('#precip')
 const timezone = document.querySelector('#timesone')
 
-//const weather_icon = document.querySelector('#weather_icon')
 const icon = document.createElement("img");
 icon.className = "icon";
-//const weather_icon = document.querySelector('#weather_icon')
 
-//'https://media.geeksforgeeks.org/wp-content/uploads/20190529122826/bs11.png';
+function fercher(location) {
+fetch('/weather?address=' + location).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      messageOne.textContent = data.error
+    } else {
+      messageOne.textContent = data.location;    
+      icon.src = data.forecast.weather_icon;
+      document.getElementById("weather_icon").appendChild(icon);
+      temp.textContent = data.forecast.temp;
+      humidity.textContent = data.forecast.humidity + '%';
+      wind_speed.textContent = data.forecast.wind_speed + ' mph';
+      precip.textContent = data.forecast.precip + '%';
+
+    }
+  })
+})
+}
+
+
+fercher('Seattle');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
-
-  fetch('/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        messageOne.textContent = data.error
-        console.log(data.error);
-      } else {
-        console.log(data.forecast.weather_icon);
-        messageOne.textContent = data.location;
-        //messageTwo.textContent = data.forecast.message;
-        
-        icon.src = data.forecast.weather_icon;
-        document.getElementById("weather_icon").appendChild(icon);
-
-        temp.textContent = data.forecast.temp;
-        //feelslike.textContent = data.feelslike;
-        humidity.textContent = "Humidity: " + data.forecast.humidity + '%';
-        wind_speed.textContent = "Wind speed: " + data.forecast.wind_speed + ' mph';
-        precip.textContent = "Precipitation: " + data.forecast.precip + '%';
-        //timezone.textContent = data.forecast.timezone;
-        console.log(data.forecast);
-      }
-    })
-  })
-
-  console.log(location)
+  this.fercher(location);
 })
+
+
+
--- src/app.js
+++ src/app.js
@@ -29,21 +29,21 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
-    name: 'Daria'
+    name: 'Daria S.'
 })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About page',
-    name: 'Daria'
+    name: 'Daria S.'
 })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help page',
-    name: 'Daria'
+    name: 'Daria S.'
 })
 })
 
